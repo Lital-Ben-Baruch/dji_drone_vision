17ff656f768894e65d479aae14e6e155537a22e7
Lital-Ben-Baruch <lital.h.ben.baruch@gmail.com>
2024-03-07 17:39:47 +0200

    Improve face detection to focus on central region
    - Added central region prioritization for face detection.
    - Visual indicators for the central area added.
    - Enhanced annotations with size and location details.

--- pid_with_camera.py
+++ pid_with_camera.py
@@ -27,12 +27,32 @@ while True:
 
     # Detect faces
     faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5)
+    # Initialize variables to keep track of the largest face
+    max_area = 0
+    largest_face = None
+    x_init = IMAGE_WIDTH // 2
+    y_init = IMAGE_HEIGHT // 2
 
-    # Draw rectangles around each face
+
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        text = f'size: w={w}, h={h} '
+        current_area = w
+        print(f'x={x}')
+        # X range: [x_init-80, x_init+80] y range: [y_init - 80, y_init+80]
+        if (x> x_init-80 and x< x_init+80) and (y> y_init -80 and y< y_init+80):
+            located_face = (x, y, w, h)
 
+    # init rect
+    IMAGE_WIDTH = 360
+    IMAGE_HEIGHT = 240
+    cv2.rectangle(img, (x_init - 40, y_init - 40), (x_init + 40, y_init + 40), (0, 0, 255), 2)
+
+
+    # Draw rectangle around  the biggest detection
+    if located_face is not None:
+        (x, y, w, h) = located_face
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        text_size = f'size: w={w}'
+        text_loc = f'loc={x, y} '
         font = cv2.FONT_HERSHEY_SIMPLEX
         # fontScale
         fontScale = 0.5
@@ -40,8 +60,8 @@ while True:
         color = (255, 0, 0)
         # Line thickness of 2 px
         thickness = 2
-        cv2.putText(img, text, (x + w//2, y), font, fontScale, color, thickness)
-
+        cv2.putText(img, text_size, (x, y - 10), font, fontScale, color, thickness)
+        cv2.putText(img, text_loc, (x, y + w + 15), font, fontScale, color, thickness)
     # Display the frame in a window named "Face Detection"
     cv2.imshow("Face Detection", img)
 
